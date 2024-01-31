e69e94b4f8c3b425b76200a452aeb4360d66ed38
Iftekharul Alam <iftekharulalam38@gmail.com>
2024-02-01 02:59:21 +0600

    bill part added

--- garments_backend_app/views/bill.py
+++ garments_backend_app/views/bill.py
@@ -12,19 +12,45 @@ from datetime import date
 @csrf_exempt
 def createBill(request):
     if request.method == 'POST':
-        shopName = request.POST.get("shopName", False)
-        date = request.POST.get("date", False)
-        productModelNo = request.POST.get("listOFProductModelNo", False)
-        productRate = request.POST.get("listOFProductRate", False)
-        productSize = request.POST.get("listOFProductSize", False)
-        productQuantity = request.POST.get("listOFProductQuantity", False)
-        totalAmount = request.POST.get("totalAmount", False)
-        billNo=1
+        listOFItem = request.POST.get("bill", False)
+        data = json.loads(listOFItem)
+        partyName = data["shopName"]
+        totalAmount = data["totalAmount"]
+        date = data["date"]
+        print(data)
+        for d in data["mybillProductsData"]:
+            
+            productModelNo = d["productModelNo"]
+            productRate = d["productRate"]
+            productSize = d["productSize"]
+            soldQuantity = d["productQuantity"]
+           
+            joma=0
+            billNo=data["billNo"]
+            print(d["productModelNo"])
+            with connection.cursor() as cursor_1:
+                cursor_1.execute("select total from product_register where productModelNo='" +str(productModelNo) + "' and productSize='" +str(productSize) + "' order by serial desc limit 1")
+                row1 = cursor_1.fetchone()
+                currentTotal = row1[0]
+                currentTotal -= int(soldQuantity) 
        
+            with connection.cursor() as cursor_2:
+                cursor_2.execute("INSERT INTO product_register(date,productModelNo,productSize,productRate,joma,total,billNo,partyName,soldQuantity,balance) VALUES ('"+str(date) + "','" +str(productModelNo) + "','" +str(
+                    productSize) + "','" +str(productRate) + "','"+str(joma) + "','" + str(currentTotal) + "','"+str(billNo) + "','"+str(partyName) + "','"+str(soldQuantity) + "','"+str(currentTotal) + "')")
+                connection.commit()
+
         with connection.cursor() as cursor_2:
-            cursor_2.execute("INSERT INTO bill(BillNo, date,shopName,productModelNo,productRate,productSize,productQuantity) VALUES ('"+str(billNo) + "','" +str(date) + "','" +str(
-                shopName) + "','" +str(productModelNo) + "','"+str(productRate) + "','" + str(productSize) + "','"+str(productQuantity) + "')")
+            cursor_2.execute("select date, khatiyanName,joma, khoroch, balance from khatiyan_full where khatiyanName='" +str(partyName) + "' order by serial desc limit 1")
+            row1 = cursor_2.fetchone()
+        balance = row1[4]
+        balance += int(totalAmount)
+        joma = 0
+        type="Party"
+
+        with connection.cursor() as cursor_3:
+            cursor_3.execute("INSERT INTO khatiyan_full(date,khatiyanName,details,joma,khoroch,balance,type) VALUES ('"+str(date) + "','" +str(partyName) + "','" +str(billNo) + "','"+str(joma) + "','"+str(totalAmount) + "','"+str(balance) + "','"+str(type) + "')")
             connection.commit()
+            
     return HttpResponse("Hello, world. You're at the polls index.")
 
 @csrf_exempt
